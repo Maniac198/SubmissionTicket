fe37eb03c3f8acad6ce48a2741788fe52f1a3efd
Maniac <mani.shc1908@gmail.com>
2024-02-23 19:46:12 +0530

    Database added

--- submissionticket.sql
+++ submissionticket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2024 at 09:47 PM
+-- Generation Time: Feb 23, 2024 at 03:15 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `auth_student`
+--
+
+CREATE TABLE `auth_student` (
+  `id` int(11) NOT NULL DEFAULT current_timestamp(),
+  `mis_id` varchar(11) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `auth_student`
+--
+
+INSERT INTO `auth_student` (`id`, `mis_id`, `password`) VALUES
+(1, 'I2K21103417', '123');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `auth_teacher`
 --
 
@@ -39,6 +58,17 @@ CREATE TABLE `auth_teacher` (
 
 INSERT INTO `auth_teacher` (`id`, `email_id`, `password`) VALUES
 (2, 'b@gmail.com', '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `auth_student`
+--
+ALTER TABLE `auth_student`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `mid_id` (`mis_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
